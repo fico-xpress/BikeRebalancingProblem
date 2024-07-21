4ed084d0f29e98cd96e85cca0b6ddde9f15da9f8
Marco <marco.deken@gmail.com>
2024-07-21 13:35:39 +0100

    Compile with c++17 to be able to use std::variant

--- .vscode/setup_env.bat
+++ .vscode/setup_env.bat
@@ -4,5 +4,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliar
 
 REM Run the make command
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x64\cl.exe" 
-REM cl /Zc:__cplusplus /nologo /EHsc /Zi /MD /wd4616 /wd4293 /I"%XPRESSDIR_CPP%\include" %1 /link /libpath:"%XPRESSDIR_CPP%\lib" xprs.lib xprscxx.lib
+REM cl /Zc:__cplusplus /std:c++17 /nologo /EHsc /Zi /MD /wd4616 /wd4293 /I"%XPRESSDIR_CPP%\include" %1 /link /libpath:"%XPRESSDIR_CPP%\lib" xprs.lib xprscxx.lib
 nmake %1 /A
